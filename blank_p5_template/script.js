c72ed09c8dea191c6e4b73279c0b0b49e8bee7df
Leon Eckert <leoneckert@mailbox.org>
2022-11-25 09:35:09 +0800

    first commit

---
+++ blank_p5_template/script.js
@@ -0,0 +1,12 @@
+console.log("my script is loaded successfully")
+
+
+function setup(){
+    let cnv = createCanvas(200, 200);
+    cnv.parent("canvasContainer")
+    background(100, 20, 230);
+}
+
+function draw(){
+
+}
